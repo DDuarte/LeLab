142cda6057b279e6fce47ddce7a32b514056db77
Nay <dnpd.dd@gmail.com>
2012-06-28 13:44:26 +0100

    Quaternion: few more typo fixes

--- src/LeEngine/Quaternion.h
+++ src/LeEngine/Quaternion.h
@@ -20,7 +20,7 @@ public:
     Quaternion(const Quaternion<T>& other) : X(other.X), Y(other.Y), Z(other.Z), W(other.W) {}
     Quaternion(const T arr[4]) : X(arr[0]), Y(arr[1]), Z(arr[2]), W(arr[3]) {}
     Quaternion(T val, T angle = 1) : X(val), Y(val), Z(val), W(angle) {}
-    Quaternion(const Vector3<T>& vec, T angle = 1) : X(vec.GetX()), Y(vec.GetY()), Z(vec.GetZ()), W(angle) {}
+    Quaternion(const Vector<3, T>& vec, T angle = 1) : X(vec.GetX()), Y(vec.GetY()), Z(vec.GetZ()), W(angle) {}
 
     T GetX() const { return X; }
     T GetY() const { return Y; }
@@ -32,17 +32,17 @@ public:
     void SetZ(T z) { Z = z; }
     void SetW(T w) { W = w; }
     void Set(T x, T y, T z, T w) { X = x; Y = y; Z = z; W = w }
-    void Set(const Vector3<T>& vec) { X = vec.GetX(); Y = vec.GetY(); Z = vec.GetZ(); }
-    void Set(const Vector3<T>& vec, T w) { Set(vec); W = w; }
+    void Set(const Vector<3, T>& vec) { X = vec.GetX(); Y = vec.GetY(); Z = vec.GetZ(); }
+    void Set(const Vector<3, T>& vec, T w) { Set(vec); W = w; }
 
     bool operator ==(const Quaternion<T>& other) const { return (X == other.X) && (Y == other.Y) && (Z == other.Z) && (W = other.W); }
     bool operator !=(const Quaternion<T>& other) const { return !operator ==(other); }
 
     Quaternion<T> operator +(const Quaternion<T>& other) const { return Quaternion<T>(X + other.X, Y + other.Y, Z + other.Z, W + other.W); }
-    Quaternion<T> operator +=(const Quaternion<T>& other) { X += other.x; Y += other.y; Z += other.z; W += other.w; return *this; }
+    Quaternion<T>& operator +=(const Quaternion<T>& other) { X += other.x; Y += other.y; Z += other.z; W += other.w; return *this; }
 
     Quaternion<T> operator -(const Quaternion<T>& other) const { return Quaternion<T>(X - other.X, Y - other.Y, Z - other.Z, W - other.W); }
-    Quaternion<T> operator -=(const Quaternion<T>& other) { X -= other.x; Y -= other.y; Z -= other.z; W -= other.w; return *this; }
+    Quaternion<T>& operator -=(const Quaternion<T>& other) { X -= other.x; Y -= other.y; Z -= other.z; W -= other.w; return *this; }
 
     Quaternion<T> operator *(const Quaternion<T>& other) const { return Quaternion<T>(W * other.X + X * other.W + Y * other.Z - Z * other.Y,
                                                                                       W * other.Y + Y * other.W + Z * other.X - X * other.Z,
@@ -50,13 +50,13 @@ public:
                                                                                       W * other.W - X * other.X - Y * other.Y - Z * other.Z); }
 
     Quaternion<T> operator *(T val) { return Quaternion<T>(X * val, Y * val, Z * val, W * val); }
-    Quaternion<T> operator *=(T val) { X *= val; Y *= val; Z *= val; W *= val; return *this; }
+    Quaternion<T>& operator *=(T val) { X *= val; Y *= val; Z *= val; W *= val; return *this; }
 
-    Vector3<T> operator *(const Vector3<T>& vec) const
+    Vector<3, T> operator *(const Vector<3, T>& vec) const
     {
         Quaternion<T> other(vec.Normalize(), 0);
         other =  other * Conjugate() * (*this);
-        return Vector3<T>(other.X, other.Y, other.Z);
+        return Vector<3, T>(other.X, other.Y, other.Z);
     }
 
     T Magnitude() const { return sqrt(X*X + Y*Y + Z*Z + W*W); }
